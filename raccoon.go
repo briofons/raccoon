44797df5682a78fb2a3450aea0adad15bb4d7b96
briofons <briofons@gmail.com>
2016-04-28 00:01:32 +0200

    First commit for raccoon's CLI

--- raccoon.go
+++ raccoon.go
@@ -5,35 +5,46 @@ import (
 	"github.com/thehivecorporation/raccoon/dispatcher"
 	"github.com/thehivecorporation/raccoon/instructions"
 	"github.com/thehivecorporation/raccoon/job"
+  	"github.com/codegangsta/cli"
+  	"os"
 )
 
 
 func main() {
-	node := connection.Node{
-		Username: "vagrant",
-		Password: "vagrant",
-		IP:       "192.168.33.10",
+
+   app := cli.NewApp()
+   app.Name = "Raccoon"
+   app.Usage = "TODO: Raccoon [options] [target [target2] ...]"
+   app.Action = func(c *cli.Context) {
+
+		node := connection.Node{
+			Username: "vagrant",
+			Password: "vagrant",
+			IP:       "192.168.33.10",
+		}
+
+		nodes := make([]connection.Node, 1)
+		nodes[0] = node
+
+		cluster := connection.Cluster{
+			Nodes: nodes,
+		}
+
+		demo_instructions := make([]instructions.Instruction, 2)
+		demo_instructions[0] = &instructions.CMD{"CMD","Install EPEL repo","sudo yum install -y epel"}
+		demo_instructions[1] = &instructions.CMD{"CMD", "Install tar", "sudo yum install -y tar"}
+
+		recipe := job.Recipe{
+			Instructions: demo_instructions,
+		}
+
+		job := job.Job{
+			Cluster: cluster,
+			Recipe:  recipe,
+		}
+
+		dispatcher.Dispatch(job)
 	}
 
-	nodes := make([]connection.Node, 1)
-	nodes[0] = node
-
-	cluster := connection.Cluster{
-		Nodes: nodes,
-	}
-
-	demo_instructions := make([]instructions.Instruction, 2)
-	demo_instructions[0] = &instructions.CMD{"CMD","Install EPEL repo","sudo yum install -y epel"}
-	demo_instructions[1] = &instructions.CMD{"CMD", "Install tar", "sudo yum install -y tar"}
-
-	recipe := job.Recipe{
-		Instructions: demo_instructions,
-	}
-
-	job := job.Job{
-		Cluster: cluster,
-		Recipe:  recipe,
-	}
-
-	dispatcher.Dispatch(job)
+	app.Run(os.Args)
 }
